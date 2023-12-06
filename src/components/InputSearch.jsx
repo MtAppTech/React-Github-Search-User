9a305235fcccf4cae10ac6de4e391468bdf05152
murat <yazilimgenel@gmail.com>
2023-12-06 22:20:48 +0100

    form error

--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -7,29 +7,40 @@ const InputSearch = ({ getData }) => {
   const [inputValue, setInputValue] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
-    //getData(inputValue);
+    getData(inputValue);
     console.log("Gönderilen Değer: ", inputValue);
   };
 
+ 
+
+ 
+  
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
     console.log(event.target.value);
   };
   return (
     <div className="row">
-      <div className="col-md-4 mx-auto" >
-        <InputGroup className="mb-3 w-100" onSubmit={handleSubmit} >
-          <Form.Control
-            value={inputValue}
-            onChange={handleInputChange}
-            placeholder="Github's username"
-            aria-label="Github's username"
-            aria-describedby="basic-addon2"
-          />
-          <Button variant="outline-secondary" id="button-addon2" type="submit">
-            Search
-          </Button>
-        </InputGroup>
+      <div className="col-md-4 mx-auto">
+      <InputGroup className="mb-3 w-100">
+            <Form.Control
+              value={inputValue}
+              onChange={handleInputChange}
+              placeholder="Github's username"
+              aria-label="Github's username"
+              aria-describedby="basic-addon2"
+            />
+            <Button
+              variant="outline-secondary"
+              id="button-addon2"
+              type="submit"
+              onClick={()=>{
+                getData(inputValue)
+              }}
+            >
+              Search
+            </Button>
+          </InputGroup>
       </div>
     </div>
   );
--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -3,16 +3,16 @@ import { Col, Card, Button, Row } from "react-bootstrap";
 
 const UserInfo = ({ dataAPI }) => {
 
-  const {name, avatar_url}=dataAPI
+  const {name,html_url, avatar_url, login}=dataAPI
   return (
     <Row xs={2} md={3} lg={3} className="mt-4 justify-content-center">
       <Col>
         <Card>
           <Card.Img variant="top" src={avatar_url} />
           <Card.Body>
-            <Card.Text> {`${dataAPI.login} (${name})`} </Card.Text>
+            <Card.Text> {`${login} (${name})`} </Card.Text>
             <Button
-              href={dataAPI.html_url}
+              href={html_url}
               target="true"
               style={{ borderRadius: "50px" }}
               variant="primary"
